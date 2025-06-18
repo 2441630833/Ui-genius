5adbadbecab6d0ee7d703af1beb86914e0a9591c
longze <longzezhu1@outlook.com>
2025-06-18 09:19:49 +0800

    feat:add communication feature between vue and iframe

--- component/VvvebJsEditor.vue
+++ component/VvvebJsEditor.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <iframe :src="pageUrl" width="100%" height="1200px" frameborder="0"></iframe>
+        <iframe ref="iframe" :src="pageUrl" width="100%" height="600px" frameborder="0"></iframe>
+        <button @click="vueSendMsg">vue向iframe传递信息</button>
+        <button @click="iframeMethods">调用iframe中的方法</button>
     </div>
 </template>
 
@@ -10,6 +12,34 @@ export default {
         return {
             pageUrl: '../static/VvvebJs/editor.html' // 指向静态文件夹中的 HTML 文件
         };
+    },
+    mounted() {
+        window.addEventListener('message', this.getiframeMsg);
+    },
+    methods: {
+        // vue获取iframe传递过来的信息
+        getiframeMsg(event) {
+                const res = event.data;
+                console.log(event)
+                if (res.cmd == 'myIframe') {
+                    console.log(res)
+                }
+            },
+            // vue向iframe传递信息
+            vueSendMsg() {
+                const iframeWindow = this.$refs.iframe.contentWindow;
+                iframeWindow.postMessage({
+                    cmd: 'myVue',
+                    params: {
+                        info: 'Vue向iframe传递的消息',
+                    }
+                }, '*')
+            },
+            // 触发iframe中的方法
+            iframeMethods() {
+                this.$refs.iframe.contentWindow.triggerByVue('通过Vue触发iframe中的方法');
+            },
+
     }
 };
 </script>
