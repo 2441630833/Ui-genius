03158870aff0ceb72d2039163f722c68661760c2
longze <longzezhu1@outlook.com>
2025-08-27 10:44:15 +0800

    chore:delete the model options redundent text

--- pages/dashboard/dashboard.vue
+++ pages/dashboard/dashboard.vue
@@ -365,7 +365,7 @@ export default {
       // Model selection
       modelOptions: [
         { value: 'gimini2.5', text: 'gimini2.5 (recommended 1 minute)', isPro: true },
-        { value: 'uigenius5:latest', text: 'uigenius5:latest (recommended 3 minutes, most powerful model)', isPro: true },
+        { value: 'uigenius5:latest', text: 'uigenius5:latest (recommended 3 minutes)', isPro: true },
         { value: 'uigenius3:basic', text: 'uigenius3:basic (free basic model, 3 minutes)', isPro: false},
       ],
       selectedModel: 'gimini2.5',
@@ -1398,6 +1398,12 @@ export default {
       }
       else if (index === 2) {
         this.selectDevice('desktop');
+      }
+      else if (index === 3) {
+        this.selectModel('uigenius5:latest')
+      }
+      else if (index === 4) {
+        this.tryExample();
       }
     }
   }
