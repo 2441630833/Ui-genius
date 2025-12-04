98d177790a7da3b9a7e1b0c65640f6f140f16b67
longze <longzezhu1@outlook.com>
2025-12-04 17:05:49 +0800

    fix:restore the env file

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 // Backend API configuration
 // production api
-// export const API_BASE_URL = 'https://aiback.uigenius.top/api'; 
+export const API_BASE_URL = 'https://aiback.uigenius.top/api'; 
 // local dev api
-export const API_BASE_URL = 'http://localhost:8000/api'; 
+// export const API_BASE_URL = 'http://localhost:8000/api'; 
