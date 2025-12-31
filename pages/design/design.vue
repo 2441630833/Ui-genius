a130f897c3061088526d37ea2df2702fc8ff795a
longze <longzezhu1@outlook.com>
2025-12-31 11:22:04 +0800

    chore: move the uigenius to bottom

--- pages/design/design.vue
+++ pages/design/design.vue
@@ -771,11 +771,11 @@ export default {
         { value: 'openai/gpt-oss-120b', text: 'openai/gpt-oss-120b', isPro: true },
         { value: 'x-ai/grok-code-fast-1', text: 'x-ai/grok-code-fast-1', isPro: true },
         { value: 'minimax/minimax-m2', text: 'minimax/minimax-m2', isPro: true },
-        { value: 'uigenius5:latest', text: 'uigenius/uigenius5:latest', isPro: false },
         { value: 'xiaomi/mimo-v2-flash:free', text: 'xiaomi/mimo-v2-flash:free', isPro: false },
         { value: 'mistralai/devstral-2512:free', text: 'mistralai/devstral-2512:free', isPro: false },
         { value: 'kwaipilot/kat-coder-pro:free', text: 'kwaipilot/kat-coder-pro:free', isPro: false },
         { value: 'google/gemma-3-27b-it:free', text: 'google/gemma-3-27b-it:free', isPro: false },
+        { value: 'uigenius5:latest', text: 'uigenius/uigenius5:latest', isPro: false }
       ],
       selectedPageModel: 'google/gemini-3-flash-preview',
       showModelDropdown: false,
